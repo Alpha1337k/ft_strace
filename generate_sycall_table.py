44fc4d4fd51b018fa3a1a7746c44b225ccf3228a
Oscar Kruithof <ozziekruithof@gmail.com>
2023-05-08 22:06:18 +0200

    Updated generation

--- generate_sycall_table.py
+++ generate_sycall_table.py
@@ -1,14 +1,18 @@
 import csv
 
+# special so https://chromium.googlesource.com/chromiumos/docs/+/master/constants/syscalls.md#x86_64-64_bit
+
 with open('x64_syscalls.csv', newline='\n') as csvFile:
 	reader = csv.DictReader(csvFile)
 	arg_keys = ['arg0', 'arg1', 'arg2', 'arg3', 'arg4', 'arg5']
+	print("#include \"src/strace.h\"")
+	print("const t_header SYSCALLS_x64[1024];")
 	for row in reader:
 		args = [];
 		for arg in arg_keys:
 			if (row[arg] == '-'): continue
 			row[arg] = 'parse_' + row[arg];
-			args.append(row[arg].replace('*', 'ptr ').replace(' ', '_').replace('?', 'unknown'))
+			args.append(row[arg].replace('*', 'ptr ').replace('const', '').replace('?', 'unknown').strip().replace(' ', '_'))
 		arg_str =  ','.join(args).replace('"', '')
 		print(f'SYSCALLS_x64[{ row["%rax"] }] = {{ "{row["syscall name"]}", {{{arg_str }}}}};')
 		# print(row);
--- src/strace.h
+++ src/strace.h
@@ -9,4 +9,19 @@
 #include <sys/user.h>
 #include <sys/syscall.h>
 
+
+typedef struct s_header
+{
+	char name[30];
+	int (*funcs[6])();
+} t_header;
+
+int parse_unsigned_int_fd();
+int parse_char_ptr_buf();
+int parse_size_t();
+int parse_char_ptr();
+int parse_char_ptr_filename();
+int parse_int_flags();
+int parse_umode_t_mode();
+
 #endif
